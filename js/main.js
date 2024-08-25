e7fc434842962469f59f44d1e0c937b400a2eb28
abishekvp <abishek.vp13@gmal.com>
2024-08-25 15:15:54 +0530

    content update

--- js/main.js
+++ js/main.js
@@ -24,20 +24,6 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
 
 
     // Header carousel
